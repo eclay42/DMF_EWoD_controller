ba4daa026c9d9d8794f5c5eb4a7c371c4b6b589c
eclay42 <eric.clay42@gmail.com>
2015-12-16 10:38:37 -0800

    fixed remaining issues with renamed project

--- DMF_EWoD_PC/DMF_EWoD_PC/Form1.Designer.vb
+++ DMF_EWoD_PC/DMF_EWoD_PC/Form1.Designer.vb
@@ -208,7 +208,7 @@ Partial Class Form1
         Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Arduino DMF Controller"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
